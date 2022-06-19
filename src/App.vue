4bcc61e01a1f943cbc153ad8cb16555caa9cb517
IbrahiimKamal <ebrahiiim.kamal@gmail.com>
2022-06-19 13:12:37 +0200

    added pages and navbar component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Navbar />
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+import Navbar from '@/components/Navbar.vue';
+</script>
+
+<style>
+@import 'bootstrap/dist/css/bootstrap.min.css';
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <router-link class="navbar-brand" to="/">Pinia</router-link>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">Features</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/register" class="nav-link">Register</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
